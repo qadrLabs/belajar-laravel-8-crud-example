11aaf93208f6db3adaeb2bb27a1cd1586009af4d
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-09-21 07:03:22 +0700

    add create new post feature

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -11,5 +12,37 @@ class PostController extends Controller
     {
         $posts = Post::latest()->get();
         return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:155',
+            'content' => 'required',
+            'status' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'status' => $request->status,
+            'slug' => Str::slug($request->title)
+        ]);
+
+        if ($post) {
+            return redirect()
+                ->route('post.index')
+                ->with(['success' => 'New post has been created successfully']);
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with(['error' => 'Some problem occurred, please try again']);
+        }
     }
 }
